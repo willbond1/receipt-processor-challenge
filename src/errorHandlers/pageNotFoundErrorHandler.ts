f4596439204f0080c9bc2a758db5bd1a501e1223
William Sullivan <willbond1@gmail.com>
2024-11-25 19:38:59 -0500

    Fixed error handler exports

--- src/errorHandlers/pageNotFoundErrorHandler.ts
+++ src/errorHandlers/pageNotFoundErrorHandler.ts
@@ -1,7 +1,5 @@
 import { Request, Response } from "express";
 
-function pageNotFoundHandler(req: Request, res: Response) {
+export function pageNotFoundHandler(req: Request, res: Response) {
     res.status(404).send('Page Not Found');
 }
-
-export { pageNotFoundHandler };
--- src/errorHandlers/serverErrorHandler.ts
+++ src/errorHandlers/serverErrorHandler.ts
@@ -1,8 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 
-function serverErrorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
+export function serverErrorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
     console.error(err.stack);
     res.status(500).send({ error: 'Internal Server Error' });
 }
-
-export { serverErrorHandler };
