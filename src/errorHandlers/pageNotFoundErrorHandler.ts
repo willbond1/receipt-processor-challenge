aa7814102ff688a14abdd033cf3442d7b01bbbc3
William Sullivan <willbond1@gmail.com>
2024-11-26 23:23:53 -0500

    Updated string quotes for consistency

--- src/errorHandlers/pageNotFoundErrorHandler.ts
+++ src/errorHandlers/pageNotFoundErrorHandler.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 
 export function pageNotFoundHandler(req: Request, res: Response) {
     res.status(404).send('Page Not Found');
--- src/errorHandlers/serverErrorHandler.ts
+++ src/errorHandlers/serverErrorHandler.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response } from 'express';
 
 export function serverErrorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
     console.error(err.stack);
